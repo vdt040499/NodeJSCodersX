a594466c1c66fd36488f62a96012ca0c4cd69fb9
VoTan <vdt040499@gmail.com>
2020-05-10 22:22:06 +0700

    View User

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,5 @@
 const db = require('../lowdb');
+const shortid = require('shortid');
 
 module.exports.index = (req, res) => {
     res.render('users/index', {
@@ -25,7 +26,18 @@ module.exports.create = (req, res) => {
     res.render('users/create');
 }
 
+module.exports.getuser = (req, res) => {
+    var id = req.params.id;
+
+    var user = db.get('users').find({ id: id }).value();
+
+    res.render('users/view', {
+        user: user
+    })
+}
+
 module.exports.createpost = (req, res) => {
+    req.body.id = shortid.generate();
     db.get('users').push(req.body).write();
     res.redirect('/users');
 }
--- routes/users.route.js
+++ routes/users.route.js
@@ -6,6 +6,8 @@ const userControllers = require('../controllers/users.controller');
 router.get('/', userControllers.index);
 router.get('/search', userControllers.search);
 router.get('/create', userControllers.create);
+router.get('/:id', userControllers.getuser);
 router.post('/create', userControllers.createpost);
+
 
 module.exports = router;
