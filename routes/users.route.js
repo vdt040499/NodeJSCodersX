6633bede434dcc6a15f1c55378c9df34e2550c56
VoTan <vdt040499@gmail.com>
2020-05-13 11:39:02 +0700

    Validation for Create user

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const userControllers = require('../controllers/users.controller');
+const validate = require('../validate/user.validate');
 
 router.get('/', userControllers.index);
 router.get('/search', userControllers.search);
 router.get('/create', userControllers.create);
 router.get('/:id', userControllers.getuser);
-router.post('/create', userControllers.createpost);
+router.post('/create',validate.postCreate, userControllers.createpost);
 
 
 module.exports = router;
